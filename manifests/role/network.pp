31f32bde45c964fe513b53d600dc172bd5d3a638
Chris Hoge <chris.hoge@puppetlabs.com>
2013-09-11 15:35:50 -0700

    -n Quantum (work in progress)

--- manifests/role/network.pp
+++ manifests/role/network.pp
@@ -2,6 +2,5 @@ class grizzly::role::network inherits role {
   include firewall::pre
   include profile::quantum::router
   include profile::quantum::agent
-  include profile::quantum::server
   include firewall::post
 }
