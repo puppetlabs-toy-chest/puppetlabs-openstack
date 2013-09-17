17f2a352a2bb78d47cc642f1346caf5da4651c9a
Chris Hoge <chris.hoge@puppetlabs.com>
2013-09-17 11:08:44 -0700

    -n Added firewall rule to allow iscsi connections to Cinder Volume.

--- manifests/profile/cinder/volume.pp
+++ manifests/profile/cinder/volume.pp
@@ -1,5 +1,22 @@
 # The profile to install the volume service
 class grizzly::profile::cinder::volume {
+
+  firewall { '03260 - iscsi API Network':
+    proto  => 'tcp',
+    state  => ['NEW'],
+    action => 'accept',
+    port   => '3260',
+    source => hiera('grizzly::network::api'),
+  }
+
+  firewall { '03260 - iscsi Management Network':
+    proto  => 'tcp',
+    state  => ['NEW'],
+    action => 'accept',
+    port   => '3260',
+    source => hiera('grizzly::network::management'),
+  }
+
   class { 'grizzly::profile::cinder::common':
     is_volume => true,
   }
