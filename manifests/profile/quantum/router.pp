075fb5972294293265140ca8a13786fb01f6605f
Chris Hoge <chris.hoge@puppetlabs.com>
2013-09-23 16:05:53 -0700

    -n Turn on ip forwarding for network node

--- manifests/profile/quantum/router.pp
+++ manifests/profile/quantum/router.pp
@@ -3,4 +3,9 @@ class grizzly::profile::quantum::router {
   class {'grizzly::profile::quantum::common':
     is_router => true,
   }
+
+  sysctl { "net.ipv4.ip_forward":
+    ensure => present,
+    value  => "1",
+  }
 }
