f9730cff561c61ed1ecded891739aff645fabba9
Cody Bunch <bunchc@gmail.com>
2013-10-15 13:31:07 -0500

    moved auth_file to profile, cleaned controller.pp

--- manifests/role/controller.pp
+++ manifests/role/controller.pp
@@ -8,9 +8,5 @@ class grizzly::role::controller inherits ::grizzly::role {
   class { '::grizzly::profile::nova::api': } ->
   class { '::grizzly::profile::quantum::server': } ->
   class { '::grizzly::profile::horizon': }
-  class { '::openstack::auth_file':
-    admin_password => hiera('grizzly::keystone::admin_password'),
-    region_name => hiera('grizzly::region'),
-    controller_node => hiera('grizzly::controller::address::api'),
-  }
+  class { '::grizzly::profile::auth_file': }
 }
