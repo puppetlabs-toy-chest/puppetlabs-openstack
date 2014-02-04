efb2ae40ed8a60780c26db63be33ae6c819d2c83
Chris Hoge <chris.hoge@puppetlabs.com>
2014-02-04 11:06:29 -0800

    Updated example to install and connect PuppetDB
    Swift requires storeconfigs, which is provided by PuppetDB.
    This update adds PuppetDB to the master environment to enable
    Swift installation.

--- examples/vagrant/10_setup_master.sh
+++ examples/vagrant/10_setup_master.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Set up the Puppet Master
-vagrant ssh puppet -c "sudo service iptables stop; \
+vagrant ssh puppet -c "sudo yum install puppetdb; \
+sudo service iptables stop; \
+sudo service puppetmaster stop; \
+sudo puppet module install puppetlabs/puppetdb; \
+sudo puppet apply --modulepath /etc/puppet/modules -e \"class { '::puppetdb': listen_address => '0.0.0.0', ssl_listen_address => '0.0.0.0' }\"; \
+sudo puppet resource package puppetdb-terminus ensure=latest; \
 sudo puppet module install puppetlabs/ntp; \
 sudo puppet module install puppetlabs/firewall; \
 sudo puppet module install puppetlabs/mysql --version 0.6.1; \
@@ -19,5 +24,9 @@ sudo mkdir /etc/puppet/hieradata; \
 sudo chown root:puppet /etc/puppet/hieradata; \
 sudo cp /etc/puppet/modules/havana/examples/common.yaml /etc/puppet/hieradata/common.yaml; \
 sudo chown root:puppet /etc/puppet/hieradata/common.yaml; \
+sudo cp /vagrant/puppet.conf /etc/puppet/puppet.conf; \
+sudo cp /vagrant/puppetdb.conf /etc/puppet/puppetdb.conf; \
+sudo cp /vagrant/routes.yaml /etc/puppet/routes.yaml; \
+sudo chown puppet:root /etc/puppet/puppet.conf /etc/puppet/puppetdb.conf /etc/puppet/routes.yaml; \
 sudo service puppetmaster start;\
 sudo puppet agent -t"
