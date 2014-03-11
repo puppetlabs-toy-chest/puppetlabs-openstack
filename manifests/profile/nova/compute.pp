d850e8e6700b14ca7747b7356cbb11ce7b056d1a
Chris Hoge <chris.hoge@puppetlabs.com>
2014-03-11 12:10:09 -0700

    Improved the firewall logic to ignore libvirt/OS
    Updated the firewall logic to identify libvirt and neutron
    rules and ignore them. Also only purges the INPUT chain to
    help preserve rules and work around firewall bugs. Removed
    unreliable exec calls and service restarts.

--- manifests/profile/nova/compute.pp
+++ manifests/profile/nova/compute.pp
@@ -12,31 +12,12 @@ class havana::profile::nova::compute {
     vncserver_listen => $management_address,
   }
 
-  # This may only be necessary for RHEL family systems
-    file { '/etc/libvirt/qemu.conf':
-      ensure => present,
-      source => 'puppet:///modules/havana/qemu.conf',
-      mode   => '0644',
-      notify => Service['libvirt'],
-    }
-    Package['libvirt'] -> File['/etc/libvirt/qemu.conf']
-
-  # clear the libvirtd masquerade rule
-  if $::osfamily == 'RedHat' {
-    # because firewall is not compatible with libvirtd, we need to flush
-    # and update rules and services
-
-    # exec rules for stopping libvirtd
-    exec { '/sbin/service libvirtd stop':
-      notify  => Service['libvirt'],
-      returns => [0, 1],
-    }
-
-    exec { '/sbin/iptables -t nat -F POSTROUTING': }
-
-    Exec['/sbin/service libvirtd stop'] ->
-    Exec['/sbin/iptables -t nat -F POSTROUTING'] ->
-    Class['::firewall']
-    Firewall['9999 - Reject remaining traffic'] -> Service['libvirt']
+  file { '/etc/libvirt/qemu.conf':
+    ensure => present,
+    source => 'puppet:///modules/havana/qemu.conf',
+    mode   => '0644',
+    notify => Service['libvirt'],
   }
+
+  Package['libvirt'] -> File['/etc/libvirt/qemu.conf']
 }
