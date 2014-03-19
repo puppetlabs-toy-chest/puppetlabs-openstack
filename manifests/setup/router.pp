a18e4e1c72bdefb3a952438bdeca9646df3c932f
Chris Hoge <chris.hoge@puppetlabs.com>
2014-03-19 15:43:53 -0700

    Automating setup of networks and images.
    Adding classes to automate the setup of public/private networks
    and loading shared test image (cirros) to help with comming
    startup tasks and Tempest testing automation.

---
+++ manifests/setup/router.pp
@@ -0,0 +1,12 @@
+define router {
+
+  $subnet = hiera('havana::network::neutron::private')
+
+  neutron_router { $title:
+    tenant_name          => $title,
+    gateway_network_name => 'public',
+    require              => [Neutron_network['public'], Neutron_subnet["${subnet}]]
+  } ->
+
+  neutron_router_interface  { "${title}:${subnet}": }
+}
