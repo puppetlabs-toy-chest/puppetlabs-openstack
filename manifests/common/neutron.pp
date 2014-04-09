puppetlabs-toy-chest/puppetlabs-havana

Enable overlapping ips for l3 network namespaces

manifests/common/neutron.pp
@@ -10,12 +10,13 @@ class havana::common::neutron {
   $data_address = ip_for_network($data_network)
 
   class { '::neutron':
-    rabbit_host     => $controller_management_address,
-    core_plugin     => 'neutron.plugins.openvswitch.ovs_neutron_plugin.OVSNeutronPluginV2',
-    rabbit_user     => hiera('havana::rabbitmq::user'),
-    rabbit_password => hiera('havana::rabbitmq::password'),
-    debug           => hiera('havana::debug'),
-    verbose         => hiera('havana::verbose'),
+    rabbit_host           => $controller_management_address,
+    core_plugin           => 'neutron.plugins.openvswitch.ovs_neutron_plugin.OVSNeutronPluginV2',
+    allow_overlapping_ips => true,
+    rabbit_user           => hiera('havana::rabbitmq::user'),
+    rabbit_password       => hiera('havana::rabbitmq::password'),
+    debug                 => hiera('havana::debug'),
+    verbose               => hiera('havana::verbose'),
   }
 
   # everone gets an ovs agent (TODO true?)

puppetlabs-toy-chest/puppetlabs-openstack

manifests/common/neutron.pp
@@ -10,12 +10,13 @@ class openstack::common::neutron {
   $data_address = ip_for_network($data_network)
 
   class { '::neutron':
-    rabbit_host     => $controller_management_address,
-    core_plugin     => 'neutron.plugins.openvswitch.ovs_neutron_plugin.OVSNeutronPluginV2',
-    rabbit_user     => hiera('openstack::rabbitmq::user'),
-    rabbit_password => hiera('openstack::rabbitmq::password'),
-    debug           => hiera('openstack::debug'),
-    verbose         => hiera('openstack::verbose'),
+    rabbit_host           => $controller_management_address,
+    core_plugin           => 'neutron.plugins.openvswitch.ovs_neutron_plugin.OVSNeutronPluginV2',
+    allow_overlapping_ips => true,
+    rabbit_user           => hiera('openstack::rabbitmq::user'),
+    rabbit_password       => hiera('openstack::rabbitmq::password'),
+    debug                 => hiera('openstack::debug'),
+    verbose               => hiera('openstack::verbose'),
   }
 
   # everone gets an ovs agent (TODO true?)