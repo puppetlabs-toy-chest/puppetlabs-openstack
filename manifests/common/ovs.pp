cb2556272d9d5665ee1fa1c78e801102b52aa319
Richard Arends <github@mosibi.nl>
2014-05-13 22:06:12 +0200

    added parameters for  'enable_tunneling', tunnel_types' and 'tenant_network_type'

--- manifests/common/ovs.pp
+++ manifests/common/ovs.pp
@@ -1,15 +1,18 @@
 class openstack::common::ovs {
   $data_network = hiera('openstack::network::data')
   $data_address = ip_for_network($data_network)
+  $enable_tunneling = hiera('openstack::neutron::tunneling', true)
+  $tunnel_types = hiera('openstack::neutron::tunnel_types', [])
+  $tenant_network_type = hiera('openstack::neutron::tenant_network_type', 'gre')
 
   class { '::neutron::agents::ovs':
-    enable_tunneling => 'True',
+    enable_tunneling => $enable_tunneling,
     local_ip         => $data_address,
     enabled          => true,
-    tunnel_types     => ['gre',],
+    tunnel_types     => $tunnel_types,
   }
 
   class  { '::neutron::plugins::ovs':
-    tenant_network_type => 'gre',
+    tenant_network_type => $tenant_network_type,
   }
 }
