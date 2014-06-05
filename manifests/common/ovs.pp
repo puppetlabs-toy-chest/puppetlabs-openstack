10a6733b990a040aaabf0f693be38054c0441a66
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2014-06-06 00:35:38 +0200

    Declared default network vlan ranges

--- manifests/common/ovs.pp
+++ manifests/common/ovs.pp
@@ -4,6 +4,7 @@ class openstack::common::ovs {
   $enable_tunneling = hiera('openstack::neutron::tunneling', true)
   $tunnel_types = hiera('openstack::neutron::tunnel_types', [])
   $tenant_network_type = hiera('openstack::neutron::tenant_network_type', 'gre')
+  $network_vlan_ranges = hiera('openstack::neutron::network_vlan_ranges', 'physnet1:1000:2000')
 
   class { '::neutron::agents::ovs':
     enable_tunneling => $enable_tunneling,
@@ -14,5 +15,6 @@ class openstack::common::ovs {
 
   class  { '::neutron::plugins::ovs':
     tenant_network_type => $tenant_network_type,
+    network_vlan_ranges => $network_vlan_ranges
   }
 }
