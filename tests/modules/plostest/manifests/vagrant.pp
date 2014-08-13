0edad80b22f388733f1ee8bfe2931a7af81c2f6a
Chris Hoge <chris.hoge@puppetlabs.com>
2014-08-13 14:52:29 -0700

    Moved scenarios and generated Puppetfile

--- tests/modules/plostest/manifests/vagrant.pp
+++ tests/modules/plostest/manifests/vagrant.pp
@@ -2,7 +2,9 @@ class plostest::vagrant {
   $testhomename = "${::env_pwd}/${::env_scenario}"
   $vagrantfilename = "${testhomename}/Vagrantfile"
   $sitefilename = "${testhomename}/site.pp"
-  $scenario = loadscenario("${::env_scenario}")
+  $puppetfilename = "${testhomename}/Puppetfile"
+  $scenario = loadscenario("scenarios/${::env_scenario}")
+  $depdata = loadscenario('openstacklibs')
   $vagrant_box  = $::env_vagrantbox
 
   file { $testhomename:
@@ -15,5 +17,10 @@ class plostest::vagrant {
   file { $sitefilename:
     ensure  => present,
     content => template('plostest/site.erb'),
+  } ->
+  file { $puppetfilename:
+    ensure  => present,
+    content => template('plostest/Puppetfile.erb'),
   }
+  
 }
