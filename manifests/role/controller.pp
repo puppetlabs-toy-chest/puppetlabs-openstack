a3bc6490361eacd6e61fff0b72f85eaec7220cfd
salmank <salmank@plumgrid.com>
2014-09-10 15:31:24 +0500

    Cirros image upload dependency removed
    Signed-off-by: salmank <salmank@plumgrid.com>

--- manifests/role/controller.pp
+++ manifests/role/controller.pp
@@ -8,11 +8,11 @@ class openstack::role::controller inherits ::openstack::role {
   class { '::openstack::profile::ceilometer::api': } ->
   class { '::openstack::profile::glance::auth': } ->
   class { '::openstack::profile::glance::api': } ->
-  class { '::openstack::setup::cirros': } ->
   class { '::openstack::profile::cinder::api': } ->
   class { '::openstack::profile::nova::api': } ->
   class { '::openstack::profile::neutron::server': } ->
   class { '::openstack::profile::heat::api': } ->
   class { '::openstack::profile::horizon': }
   class { '::openstack::profile::auth_file': }
+  class { '::openstack::setup::cirros': }
 }
