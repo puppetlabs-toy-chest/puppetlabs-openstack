1906d2be8c37a91bb232f94af15f69c7af142c98
Chris Hoge <chris@hogepodge.com>
2014-10-01 09:58:57 -0500

    Updated to build Juno on RHEL/CentOS 7
    Need CentOS 7 for testing and developing Juno release. Updating
    keys and how systems are built out using Vagrant. WIP.

--- manifests/resources/repo/epel.pp
+++ manifests/resources/repo/epel.pp
@@ -1,20 +1,20 @@
 class openstack::resources::repo::epel {
   if ($::osfamily == 'RedHat' and
       $::operatingsystem != 'Fedora' and
-      $::operatingsystemrelease =~ /^6\..*$/) {
+      $::operatingsystemrelease =~ /^7\..*$/) {
     include openstack::resources::repo::yum_refresh
 
     yumrepo { 'epel':
       mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
-      descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
+      descr          => 'Extra Packages for Enterprise Linux 7 - $basearch',
       enabled        => 1,
       gpgcheck       => 1,
-      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
       failovermethod => priority,
       notify         => Exec['yum_refresh']
     }
-    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6':
-      source => 'puppet:///modules/openstack/RPM-GPG-KEY-EPEL-6',
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7':
+      source => 'puppet:///modules/openstack/RPM-GPG-KEY-EPEL-7',
       owner  => root,
       group  => root,
       mode   => '0644',
