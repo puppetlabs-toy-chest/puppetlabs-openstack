ef2c0c52f96db52bb57a7fb2aa419dfde54c5590
guessi <guessi@gmail.com>
2015-01-12 09:55:59 +0800

    code cleanup, turn to use pick/hiera function to ensure the value is set

--- manifests/resources/connectors.pp
+++ manifests/resources/connectors.pp
@@ -6,60 +6,30 @@ class openstack::resources::connectors {
   # keystone
   $user_keystone = $::openstack::config::mysql_user_keystone
   $pass_keystone = $::openstack::config::mysql_pass_keystone
-
-  if ! $user_keystone or ! $pass_keystone {
-    $keystone   = "mysql://keystone:${password}@${management_address}/keystone"
-  } else {
-    $keystone   = "mysql://${user_keystone}:${pass_keystone}@${management_address}/keystone"
-  }
+  $keystone      = "mysql://${user_keystone}:${pass_keystone}@${management_address}/keystone"
 
   # cinder
-  $user_cinder  = $::openstack::config::mysql_user_cinder
-  $pass_cinder  = $::openstack::config::mysql_pass_cinder
-
-  if ! $user_cinder or ! $pass_cinder {
-    $cinder     = "mysql://cinder:${password}@${management_address}/cinder"
-  } else {
-    $cinder     = "mysql://${user_cinder}:${pass_cinder}@${management_address}/cinder"
-  }
+  $user_cinder   = $::openstack::config::mysql_user_cinder
+  $pass_cinder   = $::openstack::config::mysql_pass_cinder
+  $cinder        = "mysql://${user_cinder}:${pass_cinder}@${management_address}/cinder"
 
   # glance
-  $user_glance  = $::openstack::config::mysql_user_glance
-  $pass_glance  = $::openstack::config::mysql_pass_glance
-
-  if ! $user_glance or ! $pass_glance {
-    $glance     = "mysql://glance:${password}@${management_address}/glance"
-  } else {
-    $glance     = "mysql://${user_glance}:${pass_glance}@${management_address}/glance"
-  }
+  $user_glance   = $::openstack::config::mysql_user_glance
+  $pass_glance   = $::openstack::config::mysql_pass_glance
+  $glance        = "mysql://${user_glance}:${pass_glance}@${management_address}/glance"
 
   # nova
-  $user_nova    = $::openstack::config::mysql_user_nova
-  $pass_nova    = $::openstack::config::mysql_pass_nova
-
-  if ! $user_nova or ! $pass_nova {
-    $nova       = "mysql://nova:${password}@${management_address}/nova"
-  } else {
-    $nova       = "mysql://${user_nova}:${pass_nova}@${management_address}/nova"
-  }
+  $user_nova     = $::openstack::config::mysql_user_nova
+  $pass_nova     = $::openstack::config::mysql_pass_nova
+  $nova          = "mysql://${user_nova}:${pass_nova}@${management_address}/nova"
 
   # neutron
-  $user_neutron = $::openstack::config::mysql_user_neutron
-  $pass_neutron = $::openstack::config::mysql_pass_neutron
-
-  if ! $user_nova or ! $pass_nova {
-    $neutron    = "mysql://neutron:${password}@${management_address}/neutron"
-  } else {
-    $neutron    = "mysql://${user_neutron}:${pass_neutron}@${management_address}/neutron"
-  }
+  $user_neutron  = $::openstack::config::mysql_user_neutron
+  $pass_neutron  = $::openstack::config::mysql_pass_neutron
+  $neutron       = "mysql://${user_neutron}:${pass_neutron}@${management_address}/neutron"
 
   # heat
-  $user_heat    = $::openstack::config::mysql_user_heat
-  $pass_heat    = $::openstack::config::mysql_pass_heat
-
-  if ! $user_heat or ! $pass_heat {
-    $heat       = "mysql://heat:${password}@${management_address}/heat"
-  } else {
-    $heat       = "mysql://${user_heat}:${pass_heat}@${management_address}/heat"
-  }
+  $user_heat     = $::openstack::config::mysql_user_heat
+  $pass_heat     = $::openstack::config::mysql_pass_heat
+  $heat          = "mysql://${user_heat}:${pass_heat}@${management_address}/heat"
 }
