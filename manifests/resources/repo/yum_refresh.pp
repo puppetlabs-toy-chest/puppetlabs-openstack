775419b2c69a8634f2701b2582cad25315c48aa1
Colleen Murphy <colleen@puppetlabs.com>
2015-03-30 14:04:44 -0700

    Reorganize yum_refresh
    Without this patch, the yum_refresh class was only added for
    CentOS/RedHat systems and not Fedora systems because it was being
    included in the same class as epel. Since yum_refresh has nothing to do
    with epel and since yum_refresh is applicable to Fedora, this patch
    moves the yum_refresh inclusion to the repo class and does ordering
    within the yum_refresh class instead of for just the rdo repo.

--- manifests/resources/repo/yum_refresh.pp
+++ manifests/resources/repo/yum_refresh.pp
@@ -4,5 +4,5 @@ class openstack::resources::repo::yum_refresh {
     command     => '/usr/bin/yum clean all',
     refreshonly => true,
   }
-  Exec['yum_refresh'] -> Package<||>
+  Yumrepo<||> ~> Exec['yum_refresh'] -> Package<||>
 }
