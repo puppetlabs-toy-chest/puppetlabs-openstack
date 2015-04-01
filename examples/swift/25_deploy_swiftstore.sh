7a2493b48866931fa0c2d4f8b0d0e68f851b4dfd
Colleen Murphy <colleen@puppetlabs.com>
2015-03-31 17:30:58 -0700

    Use Puppet Labs CentOS 7 vagrant box in examples
    The Puppet Labs has multiple providers, so it is more accessible for
    trying out the examples.
    
    We're pinning the version to 1.0.0 because 1.0.1 seems to have issues
    with its networking interfaces.
    
    Additionally, this vagrant box comes with firewalld enabled and many
    firewall rules. Firewalld responds to iptables-save which confuses the
    firewall iptables provider and causes errors on the first round of
    puppet runs, even if disabling firewalld is puppet's first change.
    Because of this, we add a command to the deployment script to stop
    firewalld before running puppet so that puppet succeeds on the first
    run.

--- examples/swift/25_deploy_swiftstore.sh
+++ examples/swift/25_deploy_swiftstore.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 
 # Kick off the puppet runs, control is first for databases
-vagrant ssh swiftstore1 -c "sudo puppet agent -t"
-vagrant ssh swiftstore2 -c "sudo puppet agent -t"
-vagrant ssh swiftstore3 -c "sudo puppet agent -t"
+vagrant ssh swiftstore1 -c "sudo service firewalld stop; sudo puppet agent -t"
+vagrant ssh swiftstore2 -c "sudo service firewalld stop; sudo puppet agent -t"
+vagrant ssh swiftstore3 -c "sudo service firewalld stop; sudo puppet agent -t"
