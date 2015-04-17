504a1727f9ca1d4f149cc6d4578ed9c854aa08f1
Colleen Murphy <colleen@puppetlabs.com>
2015-04-17 15:15:30 -0700

    Use openstack_extras::repo instead of our own
    The openstack_extras repo can take care of installing epel/RDO and UCA
    for us.

--- manifests/resources/repo/epel.pp
+++
@@ -1,8 +0,0 @@
-class openstack::resources::repo::epel {
-  if ($::osfamily == 'RedHat' and
-      $::operatingsystem != 'Fedora' and
-      $::operatingsystemmajrelease >= 6) {
-
-    include ::epel
-  }
-}
