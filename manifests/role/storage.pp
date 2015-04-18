0fdb8128aea43d307b504c442b4cdae27ec046ac
Colleen Murphy <colleen@puppetlabs.com>
2015-04-17 21:26:03 -0700

    Remove openstack::setup::cirros class
    Having a class that sets up a single hard-coded image is not very
    useful, except for demos. This change moves the image data into the
    example hiera files and uses create_resources on a hash of data to
    create as many images as the user wants. We move this into
    openstack::profile::glance::api since now a one-line function does not
    need its own class.

--- manifests/role/storage.pp
+++ manifests/role/storage.pp
@@ -2,6 +2,4 @@ class openstack::role::storage inherits ::openstack::role {
   class { '::openstack::profile::firewall': }
   class { '::openstack::profile::glance::api': }
   class { '::openstack::profile::cinder::volume': }
-
-  class { '::openstack::setup::cirros': }
 }
