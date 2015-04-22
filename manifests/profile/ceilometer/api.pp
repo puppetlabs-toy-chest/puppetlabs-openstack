c380fbb86bfd2f2a6448dd7712c8cc89564fbc03
Colleen Murphy <colleen@puppetlabs.com>
2015-04-22 15:22:47 -0700

    Remove uncommon things from common::ceilometer
    Most of the resources in common::ceilometer are not common across nodes
    and actually have well defined places on the API server or the agent.
    We split these out and just leave the base ceilometer class, which is
    the only common class for ceilometer.

--- manifests/profile/ceilometer/api.pp
+++ manifests/profile/ceilometer/api.pp
@@ -3,9 +3,23 @@
 # after openstack::profile::ceilometer::agent
 class openstack::profile::ceilometer::api {
 
+  $mongo_username                = $::openstack::ceilometer_mongo_username
+  $mongo_password                = $::openstack::ceilometer_mongo_password
+  $ceilometer_management_address = $::openstack::ceilometer_address_management
+  $controller_management_address = $::openstack::controller_address_management
+
+
+  if ! $mongo_username or ! $mongo_password {
+    $mongo_connection = "mongodb://${ceilometer_management_address}:27017/ceilometer"
+  } else {
+    $mongo_connection = "mongodb://${mongo_username}:${mongo_password}@${ceilometer_management_address}:27017/ceilometer"
+  }
+
   openstack::resources::firewall { 'Ceilometer API':
     port => '8777',
   }
+
+  include ::openstack::common::ceilometer
 
   class { '::ceilometer::keystone::auth':
     password         => $::openstack::config::ceilometer_password,
@@ -13,6 +27,15 @@ class openstack::profile::ceilometer::api {
     admin_address    => $::openstack::config::controller_address_management,
     internal_address => $::openstack::config::controller_address_management,
     region           => $::openstack::config::region,
+  }
+
+  class { '::ceilometer::api':
+    keystone_host     => $controller_management_address,
+    keystone_password => $::openstack::ceilometer_password,
+  }
+
+  class { '::ceilometer::db':
+    database_connection => $mongo_connection,
   }
 
   class { '::ceilometer::agent::central':
@@ -36,16 +59,11 @@ class openstack::profile::ceilometer::api {
 
   class { '::ceilometer::collector': }
 
-  include ::openstack::common::ceilometer
-
   mongodb_database { 'ceilometer':
     ensure  => present,
     tries   => 20,
     require => Class['mongodb::server'],
   }
-
-  $mongo_username = $::openstack::config::ceilometer_mongo_username
-  $mongo_password = $::openstack::config::ceilometer_mongo_password
 
   if $mongo_username and $mongo_password {
     mongodb_user { $mongo_username:
