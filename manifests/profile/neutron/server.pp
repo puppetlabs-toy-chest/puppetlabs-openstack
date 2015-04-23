6056647b1650640208a284a59ef15f7f0787df3a
Colleen Murphy <colleen@puppetlabs.com>
2015-04-22 20:10:04 -0700

    Remove common::ml2 class
    The ml2 plugin only needs to be configured on the neutron server, not
    the router.

--- manifests/profile/neutron/server.pp
+++ manifests/profile/neutron/server.pp
@@ -5,7 +5,19 @@ class openstack::profile::neutron::server {
   openstack::resources::firewall { 'Neutron API': port => '9696', }
 
   include ::openstack::common::neutron
-  include ::openstack::common::ml2
+
+  $tenant_network_type           = $::openstack::config::neutron_tenant_network_type # ['gre']
+  $type_drivers                  = $::openstack::config::neutron_type_drivers # ['gre']
+  $mechanism_drivers             = $::openstack::config::neutron_mechanism_drivers # ['openvswitch']
+  $tunnel_id_ranges              = $::openstack::config::neutron_tunnel_id_ranges # ['1:1000']
+  $controller_management_address = $::openstack::config::controller_address_management
+
+  class  { '::neutron::plugins::ml2':
+    type_drivers         => $type_drivers,
+    tenant_network_types => $tenant_network_type,
+    mechanism_drivers    => $mechanism_drivers,
+    tunnel_id_ranges     => $tunnel_id_ranges
+  }
 
   Class['::neutron::db::mysql'] -> Exec['neutron-db-sync']
 }
