08ba786e5f2dfb6748a973f5e4aefd8c88250402
Colleen Murphy <colleen@puppetlabs.com>
2015-04-24 10:36:55 -0700

    Move notifications to neutron server profile
    This only needs to be configured on the controller, so let's move
    it out of "common".
    
    There should be no reason to keep the neutron server and keystone
    configs in this common class, but dhcp does not seem to work
    without it, so we'll keep it there.

--- manifests/profile/neutron/server.pp
+++ manifests/profile/neutron/server.pp
@@ -19,5 +19,14 @@ class openstack::profile::neutron::server {
     tunnel_id_ranges     => $tunnel_id_ranges
   }
 
+  anchor { 'neutron_common_first': } ->
+  class { '::neutron::server::notifications':
+    nova_url            => "http://${controller_management_address}:8774/v2/",
+    nova_admin_auth_url => "http://${controller_management_address}:35357/v2.0/",
+    nova_admin_password => $::openstack::config::nova_password,
+    nova_region_name    => $::openstack::config::region,
+  } ->
+  anchor { 'neutron_common_last': }
+
   Class['::neutron::db::mysql'] -> Exec['neutron-db-sync']
 }
