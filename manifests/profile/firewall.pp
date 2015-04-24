4dbbc598339ca2061e2cb6089e032203138b50d5
Colleen Murphy <colleen@puppetlabs.com>
2015-04-24 10:38:47 -0700

    Remove iptables-services package resource
    The firewall module takes care of this for us. Moreover, the fact
    is operatingsystemmajrelease, not operatingsystemmajversion, so this
    was never being applied.

--- manifests/profile/firewall.pp
+++ manifests/profile/firewall.pp
@@ -1,10 +1,4 @@
 class openstack::profile::firewall {
-  if $::osfamily == 'RedHat' and $::operatingsystemmajversion == 7 {
-    package { 'iptables-services':
-      ensure => present,
-    }
-    Package['iptables-services'] -> Firewall<||>
-  }
   class { '::openstack::profile::firewall::pre': }
   class { '::openstack::profile::firewall::puppet': }
   class { '::openstack::profile::firewall::post': }
