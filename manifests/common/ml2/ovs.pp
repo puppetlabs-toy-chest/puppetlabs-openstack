34042114ce84629694ad9f67699778cba45396dd
Martin Mailand <mailand@strato.de>
2015-04-29 13:32:50 +0200

    Support für l2_population

--- manifests/common/ml2/ovs.pp
+++ manifests/common/ml2/ovs.pp
@@ -5,11 +5,13 @@ class openstack::common::ml2::ovs {
   $data_address        = ip_for_network($data_network)
   $enable_tunneling    = $::openstack::config::neutron_tunneling # true
   $tunnel_types        = $::openstack::config::neutron_tunnel_types #['gre']
+  $l2_population       = $::openstack::config::neutron_l2_population
 
   class { '::neutron::agents::ml2::ovs':
     enable_tunneling => $enable_tunneling,
     local_ip         => $data_address,
     enabled          => true,
     tunnel_types     => $tunnel_types,
+    l2_population    => $l2_population,
   }
 }
