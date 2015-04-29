b1f344b7d53a28c8f87bdf560d5468f2a581e81d
Erik Wenzel <erik@as6724.net>
2015-04-29 11:00:21 +0200

    add support for vagrant provider virtualbox
    	* 05_up.sh detects installed provider and defaults vmware_fusion
    	* Vagrantfile add nic, memory and vcpu customizations for virtualbox
    	* examples/common.yaml change openstack:nova:libvirt_type to qemu

--- examples/multinode/05_up.sh
+++ examples/multinode/05_up.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 # This is the script for bringing up the standard openstack nodes without
 # Swift. This is probably the up script you want to run.
-vagrant up --provider vmware_fusion puppet control storage network compute
+
+PROVIDER=${PROVIDER:-vmware_fusion}
+
+#---  FUNCTION	----------------------------------------------------------------
+#		   NAME:  provider
+#	DESCRIPTION:  checks for binary name in PATH
+#	 PARAMETERS:  -
+#		RETURNS:  identifier for provider
+#-------------------------------------------------------------------------------
+provider ()
+{
+	if [[ -x `which vmware_fusion` ]]
+	then
+		echo "vmware_fusion"
+	elif [[ -x `which VirtualBox` ]]
+	then
+		echo "virtualbox"
+	else
+		exit 1
+	fi
+}	# ----------  end of function provider	----------
+
+vagrant up --provider $(provider) puppet control storage network compute
