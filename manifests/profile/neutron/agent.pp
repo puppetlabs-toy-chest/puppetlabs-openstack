afb643c8d05e7728cdc9e947f783e2f7aa03c2d4
Martin Mailand <mailand@strato.de>
2015-04-29 14:41:16 +0200

    Support für dvr

--- manifests/profile/neutron/agent.pp
+++ manifests/profile/neutron/agent.pp
@@ -5,6 +5,15 @@ class openstack::profile::neutron::agent {
   case $::openstack::config::neutron_core_plugin {
     'plumgrid': { include ::openstack::common::plumgrid }
     default:    { include ::openstack::common::ml2::ovs }
+  include ::openstack::common::ml2::ovs
+
+  ### Router service installation
+  class { '::neutron::agents::l3':
+    debug                    => $::openstack::config::debug,
+    external_network_bridge  => 'brex',
+    enabled                  => true,
+    agent_mode               => 'dvr',
+    router_delete_namespaces => 'True',
   }
 
 }
