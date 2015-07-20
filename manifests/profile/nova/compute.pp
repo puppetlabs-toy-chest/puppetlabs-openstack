6a87cab8131b3ab1cb205fdc08a13f76872d695f
guessi <guessi@gmail.com>
2015-07-20 15:25:15 +0800

    support for deploying ceph resource on compute node

--- manifests/profile/nova/compute.pp
+++ manifests/profile/nova/compute.pp
@@ -21,6 +21,15 @@ class openstack::profile::nova::compute {
   class { 'nova::migration::libvirt':
   }
 
+  class { '::nova::compute::rbd':
+    libvirt_rbd_user        => $::openstack::config::libvirt_rbd_user,
+    libvirt_rbd_secret_uuid => $::openstack::config::libvirt_rbd_secret_uuid,
+    libvirt_rbd_secret_key  => $::openstack::config::libvirt_rbd_secret_key,
+    libvirt_images_rbd_pool => $::openstack::config::libvirt_images_rbd_pool,
+    rbd_keyring             => $::openstack::config::rbd_keyring,
+    ephemeral_storage       => $::openstack::config::ephemeral_storage,
+  }
+
   file { '/etc/libvirt/qemu.conf':
     ensure => present,
     source => 'puppet:///modules/openstack/qemu.conf',
