5ea4f742cf3300481fc39e4d55a0a3094a7081a0
Sanjay Upadhyay <saneax@gmail.com>
2015-08-28 13:01:24 +0000

    adding vmware and virtualbox provisioner

--- examples/make_ubuntu_allinone.sh
+++ examples/make_ubuntu_allinone.sh
@@ -1,1 +1,1 @@
-OSTARGET=ubuntu VAGRANTBOX="puppetlabs/ubuntu-14-x64-openstack" SCENARIO=allinone puppet apply --modulepath=../tests/modules -e "include ::plostest::vagrant"
+OSTARGET=ubuntu VAGRANTBOX="puppetlabs/ubuntu-14-x64-openstack" SCENARIO=allinone puppet apply --debug --verbose --modulepath=../tests/modules -e "include ::plostest::vagrant"
