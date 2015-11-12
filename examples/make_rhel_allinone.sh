89cff0cbb312c75701722eb6fea7521e083cfafa
Chris Hoge <chris@openstack.org>
2015-11-12 12:04:23 -0800

    Updated module dependencies for Liberty

--- examples/make_rhel_allinone.sh
+++ examples/make_rhel_allinone.sh
@@ -1,1 +1,1 @@
-OSTARGET=rhel VAGRANTBOX="centos-7-64-openstack" SCENARIO=allinone puppet apply --modulepath=../tests/modules -e "include ::plostest::vagrant"
+OSTARGET=rhel VAGRANTBOX="centos-7-64-openstack-nov" SCENARIO=allinone puppet apply --modulepath=../tests/modules -e "include ::plostest::vagrant"
