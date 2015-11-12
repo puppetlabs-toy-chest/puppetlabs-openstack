6a8ce59902c45a643c37054e648b251aaaf36309
Chris Hoge <chris@openstack.org>
2015-11-12 15:11:33 -0800

    Added explicit domains. Restored private network
    Explicitly state 'Default' domain for users, tenants, and roles.
    Restored the previosly removed private network generation.

--- manifests/resources/tenant.pp
+++ manifests/resources/tenant.pp
@@ -1,12 +1,14 @@
 define openstack::resources::tenant (
   $description,
   $enabled = true,
+  $domain = 'Default',
 ) {
 
   keystone_tenant { $name:
     ensure      => present,
     description => $description,
     enabled     => $enabled,
+    domain      => $domain,
   }
 
 }
